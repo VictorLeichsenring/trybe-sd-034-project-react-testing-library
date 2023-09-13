07b856adc119306e3cf54de57bdcbe6f43c9624f
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-09-13 18:28:18 -0300

    Criados testes do componente NotFound

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe(' Teste o componente <NotFound.tsx />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const element = screen.getByText('Page requested not found');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toBe('H2');
+  });
+  test('Teste se a página mostra a imagem com o texto alternativo ', () => {
+    const { container } = render(<NotFound />);
+    const image = container.querySelector('img');
+    expect(image?.alt).toBe("Clefairy pushing buttons randomly with text I have no idea what i'm doing");
+  });
+});
