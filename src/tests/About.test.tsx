3dd0a3b5da2132c3ff4d0553bd499a8855f24f69
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-09-13 17:39:09 -0300

    Criados testes para o componente About

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen, render } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+describe('Teste o componente <About.tsx />.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    renderWithRouter(<About />);
+    expect(screen.getByText('About Pokédex')).toBeInTheDocument();
+  });
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    renderWithRouter(<About />);
+    const element = screen.getByText('About Pokédex');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toBe('H2');
+  });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const { container } = render(<About />);
+    const paragraphList = container.querySelectorAll('p');
+    expect(paragraphList.length).toBe(2);
+  });
+  test('Teste se a página contém a seguinte imagem de uma Pokédex: https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.', () => {
+    const url = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const { container } = render(<About />);
+    const image = container.querySelector('img');
+    expect(image?.src).toBe(url);
+  });
+});
